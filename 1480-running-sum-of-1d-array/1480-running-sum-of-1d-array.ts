c49cb3743fe2a5e03f964dcfe0c75f5f6d6fb244
cloer <83699438+cloer-Choi@users.noreply.github.com>
2022-06-29 11:03:15 -0400

    Time: 153 ms (5.14%), Space: 44.6 MB (56.42%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.ts
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.ts
@@ -1,4 +1,13 @@
+// function runningSum(nums: number[]): number[] {
+//     let sum = 0;
+//     return nums.map(num => sum += num)
+// };
+
 function runningSum(nums: number[]): number[] {
-    let sum = 0;
-    return nums.map(num => sum += num)
+    const result = [nums[0]];
+    for(let i = 1; i < nums.length; i++){
+        result[i] = result[i-1] + nums[i];
+    }
+    return result
 };
+
